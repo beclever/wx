90d03c1b7fdeb685b9ba358c629b9c8df6f919e9
beclever <849191840@qq.com>
2016-03-25 17:54:18 +0800

    准备测试号

--- wxmsdev/src/main/java/com/core/Application.java
+++ wxmsdev/src/main/java/com/core/Application.java
@@ -3,6 +3,10 @@ package com.core;
 import javax.servlet.http.HttpServletRequest;
 
 import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.WxMpServiceImpl;
+import me.chanjar.weixin.mp.bean.WxMpCustomMessage;
 import me.chanjar.weixin.mp.bean.result.WxMpOAuth2AccessToken;
 import me.chanjar.weixin.mp.bean.result.WxMpUser;
 
@@ -39,6 +43,49 @@ public class Application {
             return "code: "+ code;
             
         }
+    }
+    
+    /**
+     * 测试号
+     * @Title: mpTest
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @return
+     */
+    @RequestMapping("/mp")
+    String mpTest(){
+        WxMpInMemoryConfigStorage config = new WxMpInMemoryConfigStorage();
+        config.setAppId("wxca4549f0da3cb54f"); // 设置微信公众号的appid
+        config.setSecret("d4624c36b6795d1d99dcf0547af5443d"); // 设置微信公众号的app corpSecret
+        config.setToken("..."); // 设置微信公众号的token
+        config.setAesKey("..."); // 设置微信公众号的EncodingAESKey
+
+        WxMpService wxService = new WxMpServiceImpl();
+        wxService.setWxMpConfigStorage(config);
+
+        // 用户的openid在下面地址获得 
+        // https://mp.weixin.qq.com/debug/cgi-bin/apiinfo?t=index&type=用户管理&form=获取关注者列表接口%20/user/get 
+        String openid = "oYDxWsxrOT7YRZRJ0yR1Zy7fW2B4";//GraddyWang
+        WxMpCustomMessage message = WxMpCustomMessage.TEXT().toUser(openid).content("Hello World").build();
+        try {
+            wxService.customMessageSend(message);
+        } catch (WxErrorException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return "测试号";
+    }
+    
+    /**
+     * 
+     * @Title: kfTest
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @return
+     */
+    @RequestMapping("/kf")
+    String kfTest() {
+        
+        
+        return "多客服";
     }
     
     public static void main(String[] args) throws Exception {
